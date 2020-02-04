56ed531a2efb4e5415fb068527f6849fccc8c788
Laslaa <mohamed.laslaa@gmail.com>
2020-02-04 12:33:16 +0100

    Add the function detail

--- vendor/validator/validator.php
+++ vendor/validator/validator.php
@@ -7,7 +7,17 @@ class validator
     private $result;
     private $errors = [];
 
-    public function testInputLength($totest, $condition, $message)
+
+    /**
+     * Test the length of an input
+     *
+     * @param [type] $totest
+     * @param [type] $condition
+     * @param [type] $message
+     * @return self
+     */
+
+    public function testInputLength($totest, $condition, $message):self
     {
         if (strlen($totest) < $condition) {
             array_push($this->errors, $message);
@@ -15,7 +25,15 @@ class validator
         return $this;
     }
 
-    public function testMail($totest, $message)
+    /**
+     * Validate the format of an email
+     *
+     * @param [type] $totest
+     * @param [type] $message
+     * @return self
+     */
+
+    public function testMail($totest, $message):self
     {
         if (preg_match("/^[a-z]*([.]|\w)[a-z]*\d*[@][a-z]*[.]\w{2,5}/", $totest) == 0) {
             array_push($this->errors, $message);
@@ -23,7 +41,15 @@ class validator
         return $this;
     }
 
-    public function testPassword($totest, $message)
+    /**
+     * Test the password if it match with the regex predefined (Minimum une lettre majuscule, minimum un caractère spécial et minimum un chiffre)
+     *
+     * @param [type] $totest
+     * @param [type] $message
+     * @return self
+     */
+
+    public function testPassword($totest, $message):self
     {
         if (preg_match("/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#$%^&*()+=!?\-';,.\/{}|:<>?~]).{8,20})/", $totest) == 0) {
             array_push($this->errors, $message);
@@ -31,15 +57,32 @@ class validator
         return $this;
     }
 
-    public function inputMatchRegex($totest, $reg, $message)
+    /**
+     * Test the input if it match with the regex entered
+     *
+     * @param [type] $totest
+     * @param [type] $reg
+     * @param [type] $message
+     * @return self
+     */
+
+    public function inputMatchRegex($totest, $regex, $message):self
     {
-        if (preg_match($reg, $totest) == 0) {
+        if (preg_match($regex, $totest) == 0) {
             array_push($this->errors, $message);
         }
         return $this;
     }
 
-    public function validUrl($totest, $message)
+    /**
+     * Validate the url format provided by the users
+     *
+     * @param [type] $totest
+     * @param [type] $message
+     * @return self
+     */
+
+    public function validUrl($totest, $message):self
     {
         if (!filter_var($totest, FILTER_VALIDATE_URL)) {
             array_push($this->errors, $message);
@@ -47,19 +90,39 @@ class validator
         return $this;
     }
 
-    public function validTypeMime($totest, array $listmime, $message)
+    /**
+     * Validate the typemime of a file provided
+     *
+     * @param [type] $totest
+     * @param array $listmime
+     * @param [type] $message
+     * @return self
+     */
+
+    public function validTypeMime($totest, array $listmime, $message):self
     {
-        $mime = mime_content_type($_FILES[$totest]["tmp_name"]);
         foreach ($listmime as $val) {
-            if ($val == $mime) {
-                return $this;
+            if (isset($_POST[$totest])) {
+                $mime = mime_content_type($_FILES[$totest]["tmp_name"]);
+                if ($val == $mime) {
+                    return $this;
+                }
             }
         }
         array_push($this->errors, $message);
         return $this;
     }
 
-    public function testDate($totest, $message, $message2)
+    /**
+     * Validate the date format
+     *
+     * @param [type] $totest
+     * @param [type] $message
+     * @param [type] $message2
+     * @return self
+     */
+
+    public function testDate($totest, $message, $message2):self
     {
         if (strpos($totest, '-') !== false) {
             $test_arr = explode("-", $totest);
@@ -72,6 +135,11 @@ class validator
         return $this;
     }
 
+    /**
+     * Launch the test
+     *
+     * @return boolean
+     */
 
     public function isValid()
     {
